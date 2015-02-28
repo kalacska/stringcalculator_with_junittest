db652c9924748a78d0f86511b3f1e014eaf3c34a
Kalácska Patrik <p.kalacska@gmail.com>
2015-02-28 20:05:26 +0100

    A metódus negatív számok esetén kivételt dob.

--- stringcalculator/src/main/java/com/mycompany/stringcalculator/StringCalculator.java
+++ stringcalculator/src/main/java/com/mycompany/stringcalculator/StringCalculator.java
@@ -5,6 +5,11 @@
  */
 package com.mycompany.stringcalculator;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Patrik
@@ -14,6 +19,7 @@ public class StringCalculator {
     
     String numbersStringArray[];
     String alapertelmezett_elValasztoKarakter=",";
+    int negativTombElemSzama=0;
     
     public int add(String numbers, String elvalasztoKarakter){
         
@@ -26,8 +32,18 @@ public class StringCalculator {
             numbersStringArray=numbers.split(elvalasztoKarakter);
             
             for (String szovegesSzam : numbersStringArray) {
-                visszaAdottErtek+=Integer.valueOf(szovegesSzam);
+                
+                if (Integer.valueOf(szovegesSzam)<0) {
+                    negativTombElemSzama+=1;
+                }
+                else{
+                    visszaAdottErtek+=Integer.valueOf(szovegesSzam);
+                }
             }
+        }
+        
+        if(negativTombElemSzama>0){
+            vizsgal();
         }
  
         return visszaAdottErtek;
@@ -35,5 +51,23 @@ public class StringCalculator {
     
     public int add(String numbers){
         return this.add(numbers, alapertelmezett_elValasztoKarakter);
+    }
+
+    private void vizsgal() {
+        List<Integer> negativok=new ArrayList<>();
+        
+        for (int i = 0; i < numbersStringArray.length; i++) {
+            if (Integer.valueOf(numbersStringArray[i])<0) {
+                negativok.add(Integer.valueOf(numbersStringArray[i]));
+            }
+        }
+        
+        try {
+            throw new NegativesNotAllowed(negativok);
+        } catch (Exception ex) {
+            //Logger.getLogger(StringCalculator.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println(ex.toString());
+        }
+        
     }
 }
